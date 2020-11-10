e6d2e85002472b12d3fdc6ebca62eed73c721d06
Miguel reyes <mareyes46@misena.edu.co>
2020-11-10 16:48:01 -0500

    back ok

---
+++ back/controllers/usuarios.controller.js
@@ -0,0 +1,38 @@
+const Usuario = require("../models/User");
+
+const usuarioCtrl = {};
+
+usuarioCtrl.getUsuarios = async (req, res, next) => {
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
+};
+
+usuarioCtrl.createUsuarios = async (req, res, next) => {
+    const usuario = new Usuario({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        phone: req.body.phone,
+        email: req.body.email,
+    });
+    await usuario.save();
+    res.json({ status: "Usuario creado" });
+};
+
+usuarioCtrl.getUsuarios = async (req, res, next) => {
+    const { id } = req.params;
+    const usuario = await Usuario.findById(id);
+    res.json(usuario);
+};
+
+usuarioCtrl.editUsuarios = async (req, res, next) => {
+    const { id } = req.params;
+    await Employee.findByIdAndUpdate(id, { $set: req.body }, { new: true });
+    res.json({ status: "Usuario actualizado" });
+};
+
+usuarioCtrl.deleteUsuarios = async (req, res, next) => {
+    await Usuario.findByIdAndRemove(req.params.id);
+    res.json({ status: "usuario eliminado" });
+};
+
+module.exports = usuarioCtrl;
---
+++ back/routes/usuarios.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+const usuario = require("../controllers/usuarios.controller");
+
+router.get("/", usuario.getUsuarios);
+
+router.post("/", usuario.createUsuarios);
+
+router.get("/:id", usuario.getUsuarios);
+
+router.put("/:id", usuario.editUsuarios);
+
+router.delete("/:id", usuario.deleteUsuarios);
+
+module.exports = router
