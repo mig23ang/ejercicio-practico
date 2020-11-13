6de020f6563a79dce67157d85945c330142d152e
Miguel reyes <mareyes46@misena.edu.co>
2020-11-13 10:31:12 -0500

    tablas y paginacion ok

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
-
+import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/components/table/table.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from '../../services/usuario.service'
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnDestroy, OnInit {
+
+  dtOptions: DataTables.Settings = {};
+  dtTrigger = new Subject();
+  providers: [UsuarioService]
+  data: any;
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5
+    };
+    this.getUsuarios();
+
+  }
+  getUsuarios() {
+    this.usuarioService.getUsuarios()
+      .subscribe(res => {
+        this.usuarioService.usuarios = res as Usuario[];
+        this.dtTrigger.next();
+        console.log(res)
+      })
+  }
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
+
+}
